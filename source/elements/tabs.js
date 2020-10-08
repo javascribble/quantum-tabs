8b5d0709a1b133f31bc99fee221e7de07ed82dce
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-08 12:36:15 -0400

    working sample

--- source/elements/tabs.js
+++ source/elements/tabs.js
@@ -1,11 +1,26 @@
 import html from '../templates/tabs.js';
 
 export class Tabs extends quantum.Component {
-    constructor() {
-        super();
-    }
+    #panels = new Set();
 
     static template = quantum.template(html);
+
+    //static get observedAttributes() { return ['dock']; } // TODO: Add dock/lock controls.
+
+    slotChangedCallback(slot, addedElements, deletedElements) {
+        if (slot.name === 'tab') {
+            addedElements.forEach(element => element.addEventListener('click', this.#activate.bind(this)));
+            deletedElements.forEach(element => element.removeEventListener('click', this.#activate.bind(this)));
+        } else {
+            addedElements.forEach(element => this.#panels.add(element));
+            deletedElements.forEach(element => this.#panels.delete(element));
+        }
+    }
+
+    #activate(event) {
+        const tabName = event.target.name;
+        this.#panels.forEach(panel => quantum.setAttribute(panel, 'active', panel.getAttribute('name') === tabName));
+    }
 }
 
-quantum.define('quantum-tabs', Element);
+quantum.define('quantum-tabs', Tabs);
--- source/templates/tabs.js
+++ source/templates/tabs.js
@@ -1,5 +1,9 @@
 export default `
 <style>
-
+    ::slotted([slot="panel"]:not([active])) {
+        display: none;
+    }
 </style>
+<slot name="tab"></slot>
+<slot name="panel"></slot>
 `;
