48211cfb02e108ed4d43bfbeaf1a38e307821814
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-12 08:22:47 -0400

    change default display

--- source/templates/tabs.js
+++ source/templates/tabs.js
@@ -3,6 +3,11 @@ export default `
     ::slotted([slot="panel"]:not([active])) {
         display: none;
     }
+
+    :host {
+        display: block;
+        height: 100%;
+    }
 </style>
 <slot name="tab"></slot>
 <slot name="panel"></slot>
