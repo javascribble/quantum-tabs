f39e89cf2ebd1807bbcad8f294795116b1071431
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-11-17 12:14:14 -0500

    update dragdrop

--- test/debug.js
+++ test/debug.js
@@ -1,6 +1,27 @@
 import '/node_modules/@javascribble/quantum/source/export.js';
 import '/source/export.js';
 
-document.querySelectorAll('button').forEach(button => quantum.enableDragDrop(button));
+const dragStart = event => event.dataTransfer.setData('id', event.target.id);
+
+const dragOver = event => event.preventDefault();
+
+const drop = event => {
+    event.preventDefault();
+    const target = event.target;
+    const parent = target.parentElement;
+    const sources = document.querySelectorAll(`#${event.dataTransfer.getData('id')}`);
+    for (const source of sources) {
+        parent.insertBefore(source, target);
+    }
+};
+
+const enableDragDrop = element => {
+    element.draggable = true;
+    element.addEventListener('dragstart', dragStart);
+    element.addEventListener('dragover', dragOver);
+    element.addEventListener('drop', drop);
+};
+
+document.querySelectorAll('button').forEach(button => enableDragDrop(button));
 
 document.body.style.visibility = 'visible';
