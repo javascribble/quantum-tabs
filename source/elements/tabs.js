526199ce59a4ba65470aa9f6a7953a8085d421dd
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-16 12:24:29 -0500

    fix endless loop

--- source/elements/tabs.js
+++ source/elements/tabs.js
@@ -11,27 +11,29 @@ export class Tabs extends Component {
     createTab = (container, content) => new Tab(container, content);
 
     slotChangedCallback(slot, addedElements, deletedElements, currentElements) {
-        if (!currentElements.length) {
-            this.remove();
-        } else {
-            if (!this.active && !this.toggle) {
-                this.active = currentElements[0].id;
-            }
-
-            for (const addedElement of addedElements) {
-                const tab = this.createTab(this, addedElement);
-                const index = currentElements.indexOf(addedElement);
-                if (this.#tabs.length > index) {
-                    this.insertBefore(tab, this.#tabs[index]);
-                } else {
-                    this.appendChild(tab);
-                }
-            }
-
-            for (const deletedElement of deletedElements) {
-                this.#tabs.find(tab => tab.content.id === deletedElement.id).remove();
-                if (this.active === deletedElement.id && !this.toggle) {
+        if (!slot.name) {
+            if (!currentElements.length) {
+                this.remove();
+            } else {
+                if (!this.active && !this.toggle) {
                     this.active = currentElements[0].id;
+                }
+
+                for (const addedElement of addedElements) {
+                    const tab = this.createTab(this, addedElement);
+                    const index = currentElements.indexOf(addedElement);
+                    if (this.#tabs.length > index) {
+                        this.insertBefore(tab, this.#tabs[index]);
+                    } else {
+                        this.appendChild(tab);
+                    }
+                }
+
+                for (const deletedElement of deletedElements) {
+                    this.#tabs.find(tab => tab.content.id === deletedElement.id).remove();
+                    if (this.active === deletedElement.id && !this.toggle) {
+                        this.active = currentElements[0].id;
+                    }
                 }
             }
         }
